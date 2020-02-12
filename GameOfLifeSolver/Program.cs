9f53af01ef98321e5100c01645fc845436468306
Brandon Isbell <brandonisbell1997@gmail.com>
2020-02-11 18:48:50 -0700

    some work

--- GameOfLifeSolver/Program.cs
+++ GameOfLifeSolver/Program.cs
@@ -1,6 +1,7 @@
 ﻿using GameOfLifeSolver.Services;
 using Refit;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace GameOfLifeSolver
@@ -17,7 +18,9 @@ namespace GameOfLifeSolver
             var time = DateTime.Now.Second;
             var token = await solverService.GetToken("Bob"+random.Next().ToString());
             Console.WriteLine(token);
-            
+            List<Cell> board = null;
+            int count = 0;
+            var state = await solverService.PostUpdate(token, count);
             //game loop
             while (true)
             {
@@ -25,7 +28,17 @@ namespace GameOfLifeSolver
                 if (newTime - time >= 1)
                 {
                     time = newTime;
-                    Console.WriteLine(await solverService.PostUpdate(token, 0));
+                    state = await solverService.PostUpdate(token, count);
+                    if(board is null || board.Count == 0)
+                    {
+                        board = state.Cells;
+                    }
+                    Console.WriteLine(state.State);
+                }
+                if(state.State == "Started")
+                {
+                    board = await solverService.SolveBoard(board);
+                    count++;
                 }
             }
         }
--- GameOfLifeSolver/Services/GOLService.cs
+++ GameOfLifeSolver/Services/GOLService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,10 +20,72 @@ namespace GameOfLifeSolver.Services
             return result.Token;
         }
 
-        public async Task<string> PostUpdate(string token, int generationsComputed)
+        public async Task<(string State, List<Cell> Cells, int? Generations)> PostUpdate(string token, int generationsComputed)
         {
             var result = await _apiService.PostUpdateAsync(token, generationsComputed);
-            return result.gamestate;
+            return (result.gamestate, result.seedBoard.Select(c => new Cell(c.X, c.Y)).ToList(), result.generationsToCompute);
+        }
+
+        public async Task<List<Cell>> SolveBoard(List<Cell> board)
+        {
+            var newBoard = new List<Cell>();
+            foreach(var cell in board)
+            {
+
+            }
+            return newBoard;
+        }
+    }
+
+    public class Board
+    {
+        public Board(List<Cell> cells)
+        {
+            Cells = cells;
+        }
+        public List<Cell> Cells { get; set; }
+        public void Add(Cell cell)
+        {
+            if(Cells.Any(c=> c == cell))
+            {
+                return;
+            }
+            Cells.Add(cell);
+        }
+        public void Remove(Cell cell)
+        {
+            Cells = Cells.Where(c => c != cell).ToList();
+        }
+    }
+
+    public class Cell
+    {
+        public Cell()
+        {
+            Visited = false;
+        }
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+            Visited = false;
+        }
+        public Cell(int x, int y, bool visited)
+        {
+            X = x;
+            Y = y;
+            Visited = visited;
+        }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool Visited { get; set; } = false;
+        public static bool operator ==(Cell right, Cell left)
+        {
+            return right.X == left.X && right.Y == left.Y;
+        }
+        public static bool operator !=(Cell right, Cell left)
+        {
+            return !(right == left);
         }
     }
 }
